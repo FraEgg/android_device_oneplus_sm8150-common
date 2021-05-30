399f5236d98eb2658f63389adcd0d671f992e99f
Frank Eggen <eggen@emden.net>
2021-05-30 18:16:23 +0200

    Revert "sm8150-common: Switch to prebuilt camera provider"

--- common.mk
+++ common.mk
@@ -150,9 +150,13 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
+    Snap \
     vendor.oneplus.hardware.camera@1.0.vendor:64 \
-    vendor.qti.hardware.camera.device@1.0.vendor:64
+    vendor.oneplus.hardware.CameraMDMHIDL@1.0.vendor:64 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
 PRODUCT_PACKAGES += \
@@ -164,7 +168,6 @@ PRODUCT_PACKAGES += \
     init.oem.debug.rc \
     init.oem.rc \
     init.oem_ftm.rc \
-    init.opcamera.rc \
     init.qcom.class_core.sh \
     init.qcom.coex.sh \
     init.qcom.early_boot.sh \
@@ -324,9 +327,6 @@ PRODUCT_PACKAGES += \
     Tag \
     vendor.nxp.nxpese@1.0:64 \
     vendor.nxp.nxpnfc@1.0:64
-
-# OnePlus apps
-$(call inherit-product, vendor/oneplus/apps/config.mk)
 
 # OMX
 PRODUCT_PACKAGES += \
